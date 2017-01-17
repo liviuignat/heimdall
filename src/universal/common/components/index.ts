910f76c19794bacdddce7a7ea2c8218942459ee6
Raul Tomescu <raul.tomescu@newhaircut.com>
2017-01-17 18:41:52 +0200

    Added header and footer

--- src/universal/common/components/index.ts
+++ src/universal/common/components/index.ts
@@ -1,3 +1,6 @@
 export const FlatButton = require('material-ui/FlatButton').default;
 export const Paper = require('material-ui/Paper').default;
 export const RaisedButton = require('material-ui/RaisedButton').default;
+export const SvgIcon = require('material-ui/SvgIcon/SvgIcon').default;
+
+export const AppLogo = require('./AppLogo/AppLogo').default;
